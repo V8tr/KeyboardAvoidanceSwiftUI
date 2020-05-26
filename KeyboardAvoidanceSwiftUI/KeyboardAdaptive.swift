624a5de33342fb93a345df5be4fada72b940d340
Jeff Wear <jeff@mixmax.com>
2020-05-26 15:40:34 -0700

    Re-pad the view on re-render
    If the view re-renders, and the position of the first responder changes, while
    the keyboard is showing.

--- KeyboardAvoidanceSwiftUI/KeyboardAdaptive.swift
+++ KeyboardAvoidanceSwiftUI/KeyboardAdaptive.swift
@@ -12,24 +12,38 @@ import Combine
 /// Note that the `KeyboardAdaptive` modifier wraps your view in a `GeometryReader`, 
 /// which attempts to fill all the available space, potentially increasing content view size.
 struct KeyboardAdaptive: ViewModifier {
+    private let rerender: AnyPublisher<Any, Never>
     @State private var bottomPadding: CGFloat = 0
-
+    
+    init(rerender: AnyPublisher<Any, Never>? = nil) {
+        var publishers = [Publishers.keyboardHeight.map { arg -> Any in arg }.eraseToAnyPublisher()]
+        if let rerender = rerender {
+            publishers.append(rerender)
+        }
+        self.rerender = Publishers.MergeMany(publishers).eraseToAnyPublisher()
+    }
+    
+    private func bottomPadding(forGeometry geometry: GeometryProxy) -> CGFloat {
+        let keyboardTop = geometry.frame(in: .global).height - Publishers.keyboardHeight.value
+        var focusedTextInputBottom = UIResponder.currentFirstResponder?.globalFrame?.maxY ?? 0
+        focusedTextInputBottom += bottomPadding / 2
+        return 2 * max(0, focusedTextInputBottom - keyboardTop - geometry.safeAreaInsets.bottom)
+    }
+    
     func body(content: Content) -> some View {
         GeometryReader { geometry in
             content
                 .padding(.bottom, self.bottomPadding)
-                .onReceive(Publishers.keyboardHeight) { keyboardHeight in
-                    let keyboardTop = geometry.frame(in: .global).height - keyboardHeight
-                    let focusedTextInputBottom = UIResponder.currentFirstResponder?.globalFrame?.maxY ?? 0
-                    self.bottomPadding = max(0, focusedTextInputBottom - keyboardTop - geometry.safeAreaInsets.bottom)
-            }
-            .animation(.easeOut(duration: 0.16))
+                .onReceive(self.rerender, perform: { _ in
+                    self.bottomPadding = self.bottomPadding(forGeometry: geometry)
+                })
+                .animation(.easeOut(duration: 0.16))
         }
     }
 }
 
 extension View {
-    func keyboardAdaptive() -> some View {
-        ModifiedContent(content: self, modifier: KeyboardAdaptive())
+    func keyboardAdaptive(rerender: AnyPublisher<Any, Never>? = nil) -> some View {
+        ModifiedContent(content: self, modifier: KeyboardAdaptive(rerender: rerender))
     }
 }
